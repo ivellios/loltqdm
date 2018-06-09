52db7971fbf456613bddecd27d78bbdf151a79e8
Janusz Kamienski <janusz.kamienski@deployed.pl>
2018-06-10 01:37:31 +0200

    leave in a better place to show

--- tq2.py
+++ tq2.py
@@ -5,10 +5,10 @@ from tqdm import tqdm
 
 wishlists = random.randint(10, 20)
 
-for i in tqdm(range(wishlists), desc='Super Duper Progressbar | Processing Wishlists', unit='wishlists'):
+for i in tqdm(range(wishlists), desc='Super Duper Progressbar | Processing Wishlists', unit='wishlists', leave=False,):
     items = random.randint(20, 300)
     for j in tqdm(range(items),
                   desc='Items on wishlist {}'.format(i),
-                  leave=False,
-                  ascii=True):
+                  ascii=True,
+                  position=1):
             sleep(random.random() * 0.01)
